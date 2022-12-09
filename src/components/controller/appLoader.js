24dea37f7987495d78a0805666d90797891146f1
Sothink <rebenkov2002@mail.ru>
2022-12-09 18:24:51 +0300

    feat: add apiKey

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,9 +3,8 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'fbb371facb9f4ac499f8840136a1af78',
         });
     }
 }
-
 export default AppLoader;
